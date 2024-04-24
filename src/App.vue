90487d200f03c49e11fde6f3a34d5086581e178e
rkeenan <ross.keenan@protonmail.comm>
2024-04-23 19:12:40 -0500

    Added some markup to the components

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,25 @@
-<template>My App</template>
+<template>
+  <Header />
+  <div class="container">
+    <Balance />
+
+    <IncomeExpenses />
+    <TransactionList />
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Balance from "./components/Balance.vue";
+import IncomeExpenses from "./components/IncomeExpenses.vue";
+import TransactionList from "./components/TransactionList.vue";
+
+export default {
+  components: {
+    Header,
+    Balance,
+    IncomeExpenses,
+    TransactionList,
+  },
+};
+</script>
